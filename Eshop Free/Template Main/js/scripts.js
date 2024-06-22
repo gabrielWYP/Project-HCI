63e59faf311c748176419603096b3b4390a0b3f9
Gabriel Walter Ysidro Palomino <135283161+gabrielWYP@users.noreply.github.com>
2024-06-22 12:54:11 -0500

    Add files via upload

--- Eshop Free/Template Main/js/scripts.js
+++ Eshop Free/Template Main/js/scripts.js
@@ -10,3 +10,17 @@ function toggleAccessibilityOptions() {
     const options = document.querySelector('.accessibility-options');
     options.style.display = options.style.display === 'block' ? 'none' : 'block';
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('img');
+
+    images.forEach(image => {
+        image.addEventListener('mouseover', function() {
+            this.style.transform = 'scale(1.25)';
+        });
+
+        image.addEventListener('mouseout', function() {
+            this.style.transform = 'scale(1)';
+        });
+    });
+});
