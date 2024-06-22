20904e6bf607ff9373029eb4c00ea56ff0c1b504
Gabriel Walter Ysidro Palomino <135283161+gabrielWYP@users.noreply.github.com>
2024-06-22 15:45:04 -0500

    Delete Project-HCI-main directory

--- Project-HCI-main/src/script.js
+++
@@ -1,5 +0,0 @@
-function changeColorPalette() {
-    const select = document.getElementById('colorBlindnessType');
-    const selectedType = select.value;
-    document.body.className = selectedType;
-}
